ce801d5867b7dd9d68144d128129a84e9b52a571
jenny chen <jenny850214@yahoo.com.tw>
2017-03-10 19:37:35 +0800

    加入專案檔案。

---
+++ MyProject/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyProject.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            Models.Order order = orderService.GetOrderById("1");
+            ViewBag.CustId = order.CustId;
+            ViewBag.CustName = order.CustName;
+            return View();
+        }
+    }
+}
