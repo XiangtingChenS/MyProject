e71a02a1aabba125598bdba43164c85fb691656c
jenny chen <jenny850214@gmail.com>
2017-03-23 21:32:05 +0800

    加入專案檔案。

---
+++ MyProject/Models/OrderService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyProject.Models
+{
+    public class OrderService
+    {
+        public void InsertOrder(Models.Order order)
+        {
+
+        }
+
+
+        public void DeleteOrderById()
+        {
+
+        }
+
+        public void UpdateOrder()
+        {
+
+        }
+
+        public Models.Order GetOrderById(string Id)
+        {
+            Models.Order result = new Order();
+            result.CustId = "GSS";
+            result.CustName = "叡揚資訊";
+            return result;
+        }
+
+        public List<Models.Order> GetOrders()
+        {
+            return new List<Order>();
+        }
+    }
+}
